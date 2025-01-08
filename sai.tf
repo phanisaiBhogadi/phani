459026d684a2d5b39817fb7da7d0eca334f0ffbd
phanisaiBhogadi <phanisaibhogadi@gmail.com>
2025-01-08 16:33:40 +0530

    Update sai.tf

--- sai.tf
+++ sai.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "one" {
+resource "aws_instance" "seven" {
   ami           = "ami-0aa117785d1c1bfe5"
   instance_type = "t2.micro"
   tags = {
-    Name = kissikraja
+    Name = "saiphani"
   }
 }
