5789475877701ad110d6840ae2d1d62c9e7c0850
phanisaiBhogadi <phanisaibhogadi@gmail.com>
2025-01-08 16:31:22 +0530

    Update sai.tf

--- sai.tf
+++ sai.tf
@@ -1,27 +1,12 @@
 provider "aws" {
-region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_vpc" "one" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "dev-vpc"
-  }
-}
 
-resource "aws_subnet" "two" {
-  vpc_id     = aws_vpc.one.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "dev-subent"
-  }
-}
-
-resource "aws_instance" "three" {
-  subnet_id     = aws_subnet.two.id
-  ami           = "ami-0866a3c8686eaeeba"
+resource "aws_instance" "one" {
+  ami           = "ami-0aa117785d1c1bfe5"
   instance_type = "t2.micro"
   tags = {
-    Name = "dev-server"
-  }}
+    Name = kissikraja
+  }
+}
